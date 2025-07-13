8472f37592e3b201b8cd53d86c0adac0f47eb27b
Quoc Thanh <thanhboks128@gmail.com>
2025-07-13 23:55:58 +0700

    update route upload

--- routes/upload.js
+++ routes/upload.js
@@ -3,15 +3,20 @@ const router = express.Router();
 const upload = require('../middlewares/upload');
 const verifyToken = require('../middlewares/authMiddleware');
 
-router.post('/', verifyToken, upload.single('image'), (req, res) => {
+router.post('/', verifyToken, upload.single('image'), async (req, res) => {
+  try {
+    console.log("File nhận được:", req.file);
     if (!req.file) {
-    return res.status(400).json({ message: "Không có file được upload." });
-  }
-  // Nếu upload thành công
+      return res.status(400).json({ message: "Không có file được upload." });
+    }
     res.json({
-        message: 'Upload thành công!',
-        file: req.file
+      message: 'Upload thành công!',
+      file: req.file
     });
+  } catch (err) {
+    console.error("Upload error:", err);
+    res.status(500).json({ message: "Lỗi upload", error: err.message });
+  }
 });
 
 module.exports = router;
