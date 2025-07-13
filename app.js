76fc723c69eb0692f7b245b757b875acc7a1ec8e
Quoc Thanh <thanhboks128@gmail.com>
2025-07-13 15:06:52 +0700

    test cicd

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 // Test route
 app.get("/", (req, res) => {
-  res.send("Hello from Flutter Chat Backend!");
+  res.send("Qthanh1t's Chat App!!");
 });
 
 app.use("/api/users", userRoutes);
